694ef387414fa6931f55696e87287688e6760128
Manuroyce24 <95409974+Manuroyce24@users.noreply.github.com>
2023-05-25 21:24:35 +0200

    Delete src directory

--- src/main/java/org/thirty/app/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.thirty.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.thirty.app.model.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
